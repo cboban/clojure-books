7f8d11a27f4178967a0115b305d5bbef389a59ab
Boban Cirkovic <sjajko89@gmail.com>
2014-04-03 11:53:37 +0200

    Shelves init and improvements in navigation.

---
+++ bin/cljs/books/shelves/jsshelves.cljs
@@ -0,0 +1,6 @@
+(ns books.shelves.jsshelves
+  (:require [domina :as dom]
+	    [domina.events :as evts]
+	    [domina.css :as domcss]
+      [clojure.browser.net :as net]
+      [clojure.browser.event :as gevent]))
