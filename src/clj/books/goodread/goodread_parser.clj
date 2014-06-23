9691cb8c7a6417d3563683a17b6ce88554d11ffe
Boban Cirkovic <boban@intellex.rs>
2014-06-23 18:21:05 +0200

    Commit test.

--- src/clj/books/goodread/goodread_parser.clj
+++ src/clj/books/goodread/goodread_parser.clj
@@ -117,6 +117,7 @@
 	                   :small_image (zip-xml/text (zip-xml/xml1-> m :small_image_url))
 	                  }]
 	         (send (agent book) save-detailed-info)
+	         ;(save-detailed-info book)
 	         book
 	   ))
   )
